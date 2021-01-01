6e526a51c42cf436e3fa1a0ce2255301f47b4378
Akshay Mankar <itsakshaymankar@gmail.com>
2021-01-01 19:36:08 +0100

    Upgrade GHC to 8.10.3

--- shell.nix
+++ shell.nix
@@ -1,15 +1,17 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+  hls = pkgs.haskell-language-server.override { supportedGhcVersions = ["8103"]; };
 in
 with pkgs; mkShell {
   name = "wire-cli";
   buildInputs = [
     pkgconfig
     zlib
-    haskell.compiler.ghc884
+    haskell.compiler.ghc8103
     haskellPackages.cabal-install
     ncurses
+    hls
   ];
   shellHook = ''
     export LD_LIBRARY_PATH=${ncurses}/lib:${zlib}/lib
